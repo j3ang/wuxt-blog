e3639f59293e166f6022ed311124b645e842ae3f
alex <j3ang.info@gmail.com>
2021-01-13 15:39:25 -0600

    update composer.json

--- composer-update.sh
+++ composer-update.sh
@@ -1,10 +1,25 @@
-#!/bin/zsh 
+#!/bin/bash 
 
+sudo apt-get install jq -y
 req=$(jq '.require | keys[] as $k | "\($k)" ' composer.json)
 
 echo 'Updating composer ...'
 for k in $req; do
-    plugin=$(echo $k | cut -d '/' -f 2 | cut -d '"' -f 1)
-    #su daemon -s /bin/bash -c '/opt/bitnami/wp-cli/bin/wp plugin status $plugin'
-    [ ! -d "/path/to/dir" ] && composer require $plugin
+    # Plugin Name
+    plugin=$(echo "$k" | cut -d '/' -f 2 | cut -d '"' -f 1)
+    
+    # Vendor / Plugin Name
+    venderPlugin=$(echo "$k" | sed -e 's/^"//' -e 's/"$//')
+    
+    # active=$(su daemon -s /bin/bash -c "/opt/bitnami/wp-cli/bin/wp plugin is-active $plugin")
+    
+    echo "Checking if $plugin is installed ..."
+    if [ ! -d "/opt/bitnami/wordpress/wp-content/plugins/$plugin" ]; then
+        echo "$venderPlugin is not installed. Composer requiring ..."
+        composer require "$venderPlugin" 
+        su daemon -s /bin/bash -c "/opt/bitnami/wp-cli/bin/wp plugin activate $plugin"
+    fi
+       
+    su daemon -s /bin/bash -c "/opt/bitnami/wp-cli/bin/wp plugin activate $plugin"
+
 done
